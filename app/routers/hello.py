bbdf87a5bb8b2ec5934ddd31467cabb4b1467c7c
venushmallow <gaputri.2019@sis.smu.edu.sg>
2025-05-28 10:54:31 +0800

    feat: add post user profile endpoint

--- app/routers/hello.py
+++ app/routers/hello.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import (APIRouter, Body)
 from pathlib import Path
+from pydantic import BaseModel
 import json
 
 router = APIRouter()
@@ -8,13 +9,40 @@ router = APIRouter()
 def say_hello():
     return {"message": "Hello, world!"}
 
-@router.get("/json", tags=["Data"])
-def get_json_data():
+@router.get("/userprofiles", tags=["User Profile"])
+def get_userprofile():
     json_path = Path(__file__).parent.parent / "data" / "user-profiles.json"
     
     try:
         with open(json_path, "r", encoding="utf-8") as f:
             data = json.load(f)
+        return data
+    except FileNotFoundError:
+        return {"error": "JSON file not found."}
+    except json.JSONDecodeError:
+        return {"error": "Failed to decode JSON file."}
+
+class userRequest(BaseModel):
+    id: int
+    email: str
+    password: str
+
+@router.post("/userprofiles", tags=["User Profile"])
+def add_new_userprofile(
+    new_user: userRequest = Body(...)
+):
+    json_path = Path(__file__).parent.parent / "data" / "user-profiles.json"
+    
+    try:
+        with open(json_path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            print(data)
+        
+        data.append(new_user.model_dump())
+
+        with open(json_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
+        
         return data
     except FileNotFoundError:
         return {"error": "JSON file not found."}
